56de8e21e9227bf46f6bf6be4e7a5eb559e53183
AJS0191 <ashtonjamess@live.com>
2022-08-04 22:31:19 -0500

    EDIT: 6 BETTY

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_preorder - pre-order traversal root, left, right
- * @tree: root node 
+ * @tree: root node
  * @func: function pointer for each node
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
